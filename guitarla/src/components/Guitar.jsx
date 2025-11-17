94d3797f52f4ebe78259b2b20744c7c4fe69c055
Milton Munoz <livemilton@gmail.com>
2025-11-17 14:36:00 -0500

    project-1 react ✨ Events in JSX

--- guitarla/src/components/Guitar.jsx
+++ guitarla/src/components/Guitar.jsx
@@ -1,9 +1,13 @@
 
 
 export default function Guitar({guitar}){
-    console.log(guitar)
+    // console.log(guitar)
 
     const {id, name, image, description, price } = guitar
+    const handleClick = (id)=>{
+        console.log('Diste Click...',id)
+    }
+
     return(
           <div className="col-md-6 col-lg-4 my-4 row align-items-center">
                 <div className="col-4">
@@ -16,6 +20,7 @@ export default function Guitar({guitar}){
                     <button 
                         type="button"
                         className="btn btn-dark w-100"
+                        onClick={() => handleClick(guitar)}
                     >Agregar al Carrito</button>
                 </div>
             </div>
