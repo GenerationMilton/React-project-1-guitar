eb37697f7cb7f495a1ded3d6ce6709b3c0a9ad49
Milton Munoz <livemilton@gmail.com>
2025-11-16 22:01:57 -0500

    project-1 react ✨ show information of any guitar with props

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -25,11 +25,11 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          {data.map(()=>{
+          {data.map((guitar)=>{
             return(
               <Guitar 
-                price={100}
-                auth={true}
+              key={guitar.id}
+                guitar={guitar}
               />
             )
           })}
