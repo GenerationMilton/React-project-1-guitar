c0ad7d903f82e7f069ef3b403c67eb3cde6ded6d
Milton Munoz <livemilton@gmail.com>
2025-11-19 20:42:58 -0500

    project-1 react ✨ Create a function to increase and decrease quantity

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -10,18 +10,20 @@ function App() {
   const [data, setData] = useState(db);
   const [cart, setCart] = useState([]);
 
+  const MAX_ITEMS = 5;
+  const MIN_ITEMS = 1;
+
   function addToCart(item){
-    const itemExist = cart.findIndex((guitar =>{ guitar.id === item.id}));
-    if(itemExist >= 0){ //existe en el carrito
+    const itemExists = cart.findIndex((guitar =>{ guitar.id === item.id}));
+    if(itemExists >= 0){ //existe en el carrito
+      if(cart[itemExists].quantity >= MAX_ITEMS) return 
       const updatedCart = [...cart]
-      updatedCart[itemExist].quantity++
+      updatedCart[itemExists].quantity++
       setCart(updatedCart)
     }else {
       item.quantity = 1
-      setCart(prevCart => [...prevCart, item])
-    }
-
-    
+      setCart([...cart, item])
+    }    
   }
 
   function removeFromCart(id){
@@ -29,12 +31,41 @@ function App() {
 
   }
 
+  function increaseQuantity(id){
+    const updatedCart = cart.map( item => {
+      if( item.id === id && item.quantity < MAX_ITEMS ){
+        return {
+          ...item,
+        quantity: item.quantity +1
+        }
+
+      }
+      return item
+    })
+    setCart(updatedCart);
+  }
+
+  function decreaseQuantity(id){
+    const updatedCartDecrease = cart.map(item => {
+      if(item.id === id && item.quantity > MIN_ITEMS){
+        return{
+          ...item,
+          quantity: item.quantity -1
+        }
+      }
+      return item
+    })
+    setCart(updatedCartDecrease);
+  }
 
   return (
     <>
     <Header 
       cart={cart}
       removeFromCart={removeFromCart}
+      increaseQuantity={increaseQuantity}
+      decreaseQuantity={decreaseQuantity}
+
     />  
 
     <main className="container-xl mt-5">
--- guitarla/src/components/Header.jsx
+++ guitarla/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 import { useMemo } from "react";
 
 
-function Header({ cart, removeFromCart }) {
+function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity }) {
   //State Derivado
   const isEmpty = useMemo( () => cart.length === 0, [cart]);
   //Total to pay
@@ -57,11 +57,19 @@ function Header({ cart, removeFromCart }) {
                             <td>{guitar.name}</td>
                             <td className="fw-bold">${guitar.price}</td>
                             <td className="flex align-items-start gap-4">
-                              <button type="button" className="btn btn-dark">
+                              <button 
+                                    type="button" 
+                                    className="btn btn-dark"
+                                    onClick={()=> decreaseQuantity(guitar.id)}
+                                >
                                 -
                               </button>
                               {guitar.quantity}
-                              <button type="button" className="btn btn-dark">
+                              <button 
+                                    type="button" 
+                                    className="btn btn-dark"
+                                    onClick={()=> increaseQuantity(guitar.id)}
+                                >
                                 +
                               </button>
                             </td>
