d4699e1defd2bd9c2628e4d30d10c3506d8881b9
Milton Munoz <livemilton@gmail.com>
2025-11-23 15:37:06 -0500

    project-1 react ✨ create a Hook from cart

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -3,9 +3,12 @@ import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { useState, useEffect } from 'react'
 import { db } from "./data/db";
+import { useCart } from  "./hooks/useCart"
 
 function App() {
 
+  useCart();
+  
   const initialCart = () => {
     const localStorageCart = localStorage.getItem('cart')
     return localStorageCart ? JSON.parse(localStorageCart) : []
@@ -13,7 +16,7 @@ function App() {
 
 
   //use State
-  // const [data, setData] = useState(db);
+  const [data, setData] = useState(db);
   const [cart, setCart] = useState(initialCart);
 
   const MAX_ITEMS = 5;
---
+++ guitarla/src/hooks/useCart.js
@@ -0,0 +1,6 @@
+
+export const useCart = () => {
+
+    console.log('desde useCart')
+}
+
